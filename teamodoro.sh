308207e1c1c90a779e6cb640fac3ff33511d7fc5
Alexis Bernard <alexis@obloh.com>
2011-09-14 12:16:47 +0200

    Play a distinct sound when pomodoro is ending.

--- teamodoro.sh
+++ teamodoro.sh
@@ -2,17 +2,17 @@
 
 # Append to your crontab:
 # # m h  dom mon dow   command
-# 0,30 9,10,11,12 * * * pomodoro.sh start
-# 25,55 9,10,11,12 * * * pomodoro.sh stop 
+# 0,30 9,10,11,12 * * * teamodoro.sh start
+# 25,55 9,10,11,12 * * * teamodoro.sh stop
 
 dir=`dirname $0`
 
 case $1 in
 start)
-  play $dir/ring.wav
+  play $dir/start.wav
   ;;
 stop)
-  play $dir/ring.wav
+  play $dir/stop.wav
   ;;
 *)
   echo "Bad command, ony start and stop are accepted."
