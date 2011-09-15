e8d2490fe3ae34fc2c82ab9bd8d6aea0aefa89ab
Alexis Bernard <alexis@obloh.com>
2011-09-15 15:04:17 +0200

    Show pomodoro countdown on LED board :-)

--- teamodoro.sh
+++ teamodoro.sh
@@ -5,11 +5,21 @@
 # 0,30 9,10,11,12 * * * teamodoro.sh start
 # 25,55 9,10,11,12 * * * teamodoro.sh stop
 
+function teamodoro-show-countdown {
+  for n in `seq 25 -1 0`; do
+    dcled -m "POMODORO $n min left" -r &
+    pid=$!
+    sleep 60
+    kill $pid
+  done
+}
+
 dir=`dirname $0`
 
 case $1 in
 start)
   play $dir/start.wav
+  teamodoro-show-countdown
   ;;
 stop)
   play $dir/stop.wav
